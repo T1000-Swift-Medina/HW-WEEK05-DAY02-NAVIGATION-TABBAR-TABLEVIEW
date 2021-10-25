8e99c66c98c3a74b5b17bc838fda6815d6b27ce6
RazanAlthkafi <Razan.Althkafi.med.s@tuwaiq.edu.sa>
2021-10-25 22:35:11 +0300

    Initial Commit

---
+++ AlbaikHomework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlbaikHomework
+//
+//  Created by Roaa aljohani on 19/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
