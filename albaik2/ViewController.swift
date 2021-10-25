ff4a70aff669571b1f63a1e8cc1b89ee69aa0c03
yazzan-96 <yazanaraddadi@gmail.com>
2021-10-25 14:36:18 +0300

    Initial Commit

---
+++ albaik2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  albaik2
+//
+//  Created by Yazan Alraddadi on 19/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
