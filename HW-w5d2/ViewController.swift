002b95b5a70306a26d93b645e4af70f6ba24265a
Osama mahmoud Fallatah <Osama.Fallatah.med.s@tuwaiq.edu.sa>
2021-10-25 13:12:33 +0300

    Initial Commit

---
+++ HW-w5d2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW-w5d2
+//
+//  Created by Osama folta on 19/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
