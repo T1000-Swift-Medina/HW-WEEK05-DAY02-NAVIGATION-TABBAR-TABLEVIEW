c76cb5cefd3d59dc2468151d3e36ca65c721f3ed
Khalid Alshammeri <khalid.Ali.med.s@tuwaiq.edu.sa>
2021-10-25 12:40:37 +0300

    Initial Commit

---
+++ HW.W5-D2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW.W5-D2
+//
+//  Created by khalid ali on 19/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
