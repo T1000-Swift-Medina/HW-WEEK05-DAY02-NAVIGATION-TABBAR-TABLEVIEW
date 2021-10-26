ad220dc331f0ce9f4dc17956f6bdd197519d86b4
NUJUD3 <nojoudmohaimmed@hotmail.com>
2021-10-26 12:14:04 +0300

    Initial Commit

---
+++ AlbaikNav/MyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyTableViewCell.swift
+//  AlbaikNav
+//
+//  Created by Nojoud Mohaimmed on 20/03/1443 AH.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
