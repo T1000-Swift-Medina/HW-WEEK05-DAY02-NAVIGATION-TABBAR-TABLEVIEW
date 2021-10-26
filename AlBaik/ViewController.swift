b152d90e2f6f237ccc1e74037c0494e10ed746a9
ReemSaleh <reem.saleh.med.s@tuwaiq.edu.sa>
2021-10-26 14:23:39 +0300

    Initial Commit

---
+++ AlBaik/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlBaik
+//
+//  Created by Reem Saleh on 20/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
