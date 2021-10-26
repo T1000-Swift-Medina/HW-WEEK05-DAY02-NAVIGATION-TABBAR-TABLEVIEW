1569d306269359a57b29e672e7db708297b492c7
ReemSaleh <reem.saleh.med.s@tuwaiq.edu.sa>
2021-10-26 14:47:09 +0300

    Hw

---
+++ AlBaik/ProuductCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProuductCell.swift
+//  ex31
+//
+//  Created by Reem Saleh on 20/03/1443 AH.
+//
+
+import UIKit
+
+class ProuductCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
