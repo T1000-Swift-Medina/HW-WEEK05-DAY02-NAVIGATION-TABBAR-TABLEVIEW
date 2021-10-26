52b24409e57ef11abfa47412992dea8b8da0f15f
Noura.alahmadi.med.s@tuwaiq.edu.sa <Teeko1122>
2021-10-26 12:15:31 +0300

    Initial Commit

---
+++ Resturant/BannerTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BannerTableViewCell.swift
+//  Resturant
+//
+//  Created by Noura Alahmadi on 20/03/1443 AH.
+//
+
+import UIKit
+
+class BannerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Resturant/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Resturant
+//
+//  Created by Noura Alahmadi on 19/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
+
