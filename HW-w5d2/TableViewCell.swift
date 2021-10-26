592bcc6f9eab63d61af23d1cf0dcbee6cb81f0e2
Osama mahmoud Fallatah <Osama.Fallatah.med.s@tuwaiq.edu.sa>
2021-10-26 21:59:58 +0300

    Update al-biake

---
+++ HW-w5d2/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  HW-w5d2
+//
+//  Created by Osama folta on 20/03/1443 AH.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ HW-w5d2/TableViewCell2.swift
@@ -0,0 +1,34 @@
+//
+//  TableViewCell2.swift
+//  HW-w5d2
+//
+//  Created by Osama folta on 20/03/1443 AH.
+//
+
+import UIKit
+
+class TableViewCell2: UITableViewCell {
+    
+    @IBOutlet weak var cellimg: UIImageView!
+    
+    @IBOutlet weak var titl: UILabel!
+    @IBOutlet weak var descrb: UILabel!
+    
+    @IBOutlet weak var cost: UILabel!
+    
+    @IBAction func btn(_ sender: Any) {
+    }
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
